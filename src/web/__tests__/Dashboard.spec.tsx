8376d48b3c66fbbf2cbf6e4772def620ebca590c
Will Adams <will.adams@looker.com>
2020-03-25 15:05:30 -0700

    use enzyme to test hooks

--- src/web/__tests__/Dashboard.spec.tsx
+++ src/web/__tests__/Dashboard.spec.tsx
@@ -7,25 +7,31 @@ import { expect } from 'chai'
 import Enzyme, { mount, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import {act} from 'react-dom/test-utils'
+import { Button } from '@material-ui/core'
+import {Post} from '../dashboard/types'
+import { Posts } from '../dashboard/Posts'
 
 Enzyme.configure({ adapter: new Adapter()})
 
 describe('User Dashboard', () => {
   beforeEach(() => {
+    const posts: Post[] = [
+      {
+        text: 'first post',
+        userId: '1',
+      },
+      {
+        text: 'second post',
+        userId: '1'
+      }
+    ]
     sinon.stub(api, 'getUserPosts').resolves({
-      json: () => Promise.resolve([
-        {
-          text: 'first post',
-          postedBy: '1'
-        },
-        {
-          text: 'second post',
-          postedBy: '1'
-        }
-      ])
+      json: () => Promise.resolve(posts)
     })
 
-    sinon.stub(api, 'postUserPost').resolves()
+    sinon.stub(api, 'postUserPost').callsFake(async (post: Post) => {
+      posts.push(post)
+    })
   })
 
   afterEach(sinon.restore)
@@ -36,8 +42,15 @@ describe('User Dashboard', () => {
     }).not.throw()
   })
 
-  it('Should mount a child PostComposer that increments postCount via its callback', async () => {
-    const dashWrapper = mount(<Dashboard userId={'1'}/>)
-    await act(async () => dashWrapper.find('button').first().simulate('click'))
+  it('Should mount a child PostComposer', async () => {
+    const callback = sinon.stub()
+    sinon.stub(React, 'useState').callsFake((arg: any) => [arg, callback])
+    const dashWrapper = mount(<Dashboard userId={'1'} />)
+    await act(() => Promise.resolve(dashWrapper))
+    const buttons = dashWrapper.find(Button)
+    const button = buttons.first()
+    await act(async () => button.simulate('click'))
+    expect(dashWrapper.find(Posts).length).to.be.above(0)
+    expect(dashWrapper.find('li').length).to.be.above(2)
   })
 })
